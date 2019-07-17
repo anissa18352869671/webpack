5ea24ebe41184b6f7fece99a5289eb1ed77dec8e
anissa0511 <dingziling@shunwang.com>
2019-07-17 14:51:51 +0800

    新增

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>HELLO {{name}}</div>
+    <div>AAS</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Vue.js"
+    };
+  }
+};
+</script>
+<style scoped>
+div {
+  color: #f60;
+  font-size: 24px;
+}
+</style>
