a6c1a87db127f217206fe54addb207118e101e63
anissa0511 <dingziling@shunwang.com>
2019-07-18 14:06:37 +0800

    更新组件

--- app.vue
+++ app.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <div>HELLO {{name}}</div>
-    <div>{{data}}</div>
+    <v-title title="Vue组件化"></v-title>
+    <v-button @click="handleClick">点击按钮</v-button>
   </div>
 </template>
 <script>
+import vTitle from "./title.vue";
+import vButton from "./button.vue";
 export default {
-  data() {
-    return {
-      name: "Anissa today",
-      data: "啊飒飒!"
-    };
+  components: {
+    vTitle,
+    vButton
+  },
+  methods: {
+    handleClick(e) {
+      console.log(e);
+    }
   }
 };
 </script>
---
+++ button.vue
@@ -0,0 +1,47 @@
+<template>
+  <button @click="handleClick" :style="styles">
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "#00cc66"
+    }
+  },
+  data() {
+    return {
+      myColor: this.color
+    };
+  },
+  computed: {
+    styles() {
+      return {
+        background: this.myColor
+      };
+    }
+  },
+  methods: {
+    handleClick(e) {
+      this.myColor = "red";
+      this.$emit("click", e);
+      //   console.log(this);
+    }
+  }
+};
+</script>
+<style scoped>
+button {
+  border: 0;
+  outline: none;
+  color: #fff;
+  padding: 4px 8px;
+}
+button:active {
+  position: relative;
+  top: 1px;
+  left: 1px;
+}
+</style>
